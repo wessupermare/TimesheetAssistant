d9abad61741dce0499b0e1f9c7a764e46be83993
Weston Sleeman <weston@live.com>
2017-09-24 10:51:39 -0700

    Added quick switch feature.

--- TimesheetAssistant/FrmMain.Designer.cs
+++ TimesheetAssistant/FrmMain.Designer.cs
@@ -34,6 +34,7 @@
             this.BtnShowLogs = new System.Windows.Forms.Button();
             this.BtnSettings = new System.Windows.Forms.Button();
             this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.LbxProjects = new System.Windows.Forms.ListBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,12 +47,14 @@
             this.tableLayoutPanel1.Controls.Add(this.BtnTime, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.BtnShowLogs, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.BtnSettings, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.LbxProjects, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(280, 57);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 110F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(280, 143);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // BtnTime
@@ -59,7 +62,7 @@
             this.BtnTime.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BtnTime.Location = new System.Drawing.Point(3, 3);
             this.BtnTime.Name = "BtnTime";
-            this.BtnTime.Size = new System.Drawing.Size(87, 51);
+            this.BtnTime.Size = new System.Drawing.Size(87, 27);
             this.BtnTime.TabIndex = 0;
             this.BtnTime.Text = "Start";
             this.BtnTime.UseVisualStyleBackColor = true;
@@ -70,7 +73,7 @@
             this.BtnShowLogs.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BtnShowLogs.Location = new System.Drawing.Point(96, 3);
             this.BtnShowLogs.Name = "BtnShowLogs";
-            this.BtnShowLogs.Size = new System.Drawing.Size(87, 51);
+            this.BtnShowLogs.Size = new System.Drawing.Size(87, 27);
             this.BtnShowLogs.TabIndex = 2;
             this.BtnShowLogs.Text = "Logs";
             this.BtnShowLogs.UseVisualStyleBackColor = true;
@@ -81,7 +84,7 @@
             this.BtnSettings.Dock = System.Windows.Forms.DockStyle.Fill;
             this.BtnSettings.Location = new System.Drawing.Point(189, 3);
             this.BtnSettings.Name = "BtnSettings";
-            this.BtnSettings.Size = new System.Drawing.Size(88, 51);
+            this.BtnSettings.Size = new System.Drawing.Size(88, 27);
             this.BtnSettings.TabIndex = 3;
             this.BtnSettings.Text = "Settings";
             this.BtnSettings.UseVisualStyleBackColor = true;
@@ -92,11 +95,23 @@
             this.Timer.Interval = 60000;
             this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
             // 
+            // LbxProjects
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.LbxProjects, 3);
+            this.LbxProjects.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LbxProjects.FormattingEnabled = true;
+            this.LbxProjects.Location = new System.Drawing.Point(3, 36);
+            this.LbxProjects.Name = "LbxProjects";
+            this.LbxProjects.Size = new System.Drawing.Size(274, 104);
+            this.LbxProjects.TabIndex = 4;
+            this.LbxProjects.SelectedIndexChanged += new System.EventHandler(this.LbxProjects_SelectedIndexChanged);
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(280, 57);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(280, 143);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.MaximizeBox = false;
@@ -117,6 +132,7 @@
         private System.Windows.Forms.Button BtnShowLogs;
         private System.Windows.Forms.Timer Timer;
         private System.Windows.Forms.Button BtnSettings;
+        private System.Windows.Forms.ListBox LbxProjects;
     }
 }
 
--- TimesheetAssistant/FrmMain.cs
+++ TimesheetAssistant/FrmMain.cs
@@ -36,6 +36,7 @@ namespace TimesheetAssistant
                 }
                 Settings.Reload();
             }
+            UpdateListbox();
         }
 
         private void BtnSettings_Click(object sender, EventArgs e)
@@ -51,66 +52,77 @@ namespace TimesheetAssistant
         {
             if (BtnTime.Text == "Start")
             {
-                ActiveProject = Prompt.ShowDialog("What project are you working on?", "Select Active Project", Settings.Projects.ToArray());
-                if (ActiveProject == "") return;
-
-                BtnTime.Text = "Stop";
-                Timer.Interval = (int)new TimeSpan(0, (int)Settings.TrackInterval, 0).TotalMilliseconds;
-                SessionStartTime = DateTime.Now;
-                Timer.Enabled = true;
+                Start((string)LbxProjects.SelectedItem);
             }
             else
             {
-                Timer.Enabled = false;
-                DateTime SessionEndTime = DateTime.Now;
-                BtnTime.Text = "Start";
-                Dictionary<string, List<TimeEntry>> tmpDic = Settings.TimeSpent;
-                List<TimeEntry> tmpList = null;
+                Stop();
+            }
+        }
 
+        private void Start(string proj)
+        {
+            ActiveProject = proj;
+            if (ActiveProject == "") return;
+
+            BtnTime.Text = "Stop";
+            Timer.Interval = (int)new TimeSpan(0, (int)Settings.TrackInterval, 0).TotalMilliseconds;
+            SessionStartTime = DateTime.Now;
+            Timer.Enabled = true;
+        }
+
+        private void Stop()
+        {
+            Timer.Enabled = false;
+            DateTime SessionEndTime = DateTime.Now;
+            BtnTime.Text = "Start";
+            Dictionary<string, List<TimeEntry>> tmpDic = Settings.TimeSpent;
+            List<TimeEntry> tmpList = null;
+
+            try
+            {
+                tmpList = tmpDic[$"{ActiveProject};{DateTime.Today.Date}"];
+                tmpList.Add(new TimeEntry { StartTime = SessionStartTime, Duration = TimeSpan.FromMinutes((int)Math.Round(SessionEndTime.Subtract(SessionStartTime).TotalMinutes, 0)) });
+                tmpDic[$"{ActiveProject};{DateTime.Today.Date}"] = tmpList;
+                Settings.TimeSpent = tmpDic;
+            }
+            catch
+            {
                 try
                 {
-                    tmpList = tmpDic[$"{ActiveProject};{DateTime.Today.Date}"];
-                    tmpList.Add(new TimeEntry { StartTime = SessionStartTime, Duration = TimeSpan.FromMinutes((int)Math.Round(SessionEndTime.Subtract(SessionStartTime).TotalMinutes, 0)) });
-                    tmpDic[$"{ActiveProject};{DateTime.Today.Date}"] = tmpList;
+                    tmpList = new List<TimeEntry> { new TimeEntry { StartTime = SessionStartTime, Duration = TimeSpan.FromMinutes((int)Math.Round(SessionEndTime.Subtract(SessionStartTime).TotalMinutes, 0)) } };
+                    tmpDic.Add($"{ActiveProject};{DateTime.Today.Date}", tmpList);
                     Settings.TimeSpent = tmpDic;
                 }
                 catch
                 {
-                    try
-                    {
-                        tmpList = new List<TimeEntry> { new TimeEntry { StartTime = SessionStartTime, Duration = TimeSpan.FromMinutes((int)Math.Round(SessionEndTime.Subtract(SessionStartTime).TotalMinutes, 0)) } };
-                        tmpDic.Add($"{ActiveProject};{DateTime.Today.Date}", tmpList);
-                        Settings.TimeSpent = tmpDic;
-                    }
-                    catch
-                    {
-                        Settings.TimeSpent = new Dictionary<string, List<TimeEntry>>
+                    Settings.TimeSpent = new Dictionary<string, List<TimeEntry>>
                         {
                             { $"{ActiveProject};{DateTime.Today.Date}", tmpList }
                         };
-                    }
                 }
+            }
 
-                Dictionary<string, List<Log>> tmpLogDic = Settings.Logs;
-                List<Log> tmpLogList = null;
+            Dictionary<string, List<Log>> tmpLogDic = Settings.Logs;
+            List<Log> tmpLogList = null;
+            try
+            {
+                tmpLogList = tmpLogDic[$"{ActiveProject};{DateTime.Today.Date}"];
+                tmpLogList.Add(new Log(Prompt.ShowDialog($"What have you done since {SessionStartTime.Hour}:{SessionStartTime.Minute}?", $"Log Entry For {DateTime.Now}"), ActiveProject, new TimeEntry { StartTime = SessionStartTime, Duration = TimeSpan.FromMinutes((int)Math.Round(SessionEndTime.Subtract(SessionStartTime).TotalMinutes, 0)) }));
+                tmpLogDic[$"{ActiveProject};{DateTime.Today.Date}"] = tmpLogList;
+                Settings.Logs = tmpLogDic;
+            }
+            catch
+            {
                 try
                 {
-                    tmpLogList = tmpLogDic[$"{ActiveProject};{DateTime.Today.Date}"];
-                    tmpLogList.Add(new Log(Prompt.ShowDialog($"What have you done since {SessionStartTime.Hour}:{SessionStartTime.Minute}?", $"Log Entry For {DateTime.Now}"), ActiveProject, new TimeEntry { StartTime = SessionStartTime, Duration = TimeSpan.FromMinutes((int)Math.Round(SessionEndTime.Subtract(SessionStartTime).TotalMinutes, 0)) }));
-                    tmpLogDic[$"{ActiveProject};{DateTime.Today.Date}"] = tmpLogList;
+                    tmpLogList = new List<Log> { new Log(Prompt.ShowDialog($"What have you done since {SessionStartTime.Hour}:{SessionStartTime.Minute}?", $"Log Entry For {DateTime.Now}"), ActiveProject, new TimeEntry { StartTime = SessionStartTime, Duration = TimeSpan.FromMinutes((int)Math.Round(SessionEndTime.Subtract(SessionStartTime).TotalMinutes, 0)) }) };
+                    tmpLogDic.Add($"{ActiveProject};{DateTime.Today.Date}", tmpLogList);
                     Settings.Logs = tmpLogDic;
                 }
                 catch
                 {
-                    try
-                    {
-                        tmpLogList = new List<Log> { new Log(Prompt.ShowDialog($"What have you done since {SessionStartTime.Hour}:{SessionStartTime.Minute}?", $"Log Entry For {DateTime.Now}"), ActiveProject, new TimeEntry { StartTime = SessionStartTime, Duration = TimeSpan.FromMinutes((int)Math.Round(SessionEndTime.Subtract(SessionStartTime).TotalMinutes, 0)) }) };
-                        tmpLogDic.Add($"{ActiveProject};{DateTime.Today.Date}", tmpLogList);
-                        Settings.Logs = tmpLogDic;
-                    }
-                    catch
-                    {
-                        Settings.Logs = new Dictionary<string, List<Log>>
+                    Settings.Logs = new Dictionary<string, List<Log>>
                         {
                             { $"{ActiveProject};{DateTime.Today.Date}", new List<Log>
                             {
@@ -122,11 +134,10 @@ namespace TimesheetAssistant
                             }
                             }
                         };
-                    }
                 }
-                Settings.Save();
-                Settings.Reload();
             }
+            Settings.Save();
+            Settings.Reload();
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -200,6 +211,17 @@ namespace TimesheetAssistant
             Timer.Enabled = true;
         }
 
+        private void UpdateListbox()
+        {
+            LbxProjects.Items.Clear();
+            LbxProjects.BeginUpdate();
+            foreach (string proj in Settings.Projects)
+            {
+                LbxProjects.Items.Add(proj);
+            }
+            LbxProjects.EndUpdate();
+        }
+
         private void BtnShowLogs_Click(object sender, EventArgs e)
         {
             using (FrmLogs frmLogs = new FrmLogs())
@@ -211,6 +233,15 @@ namespace TimesheetAssistant
         private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
         {
             Environment.Exit(0);
+        }
+
+        private void LbxProjects_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (BtnTime.Text != "Start")
+            {
+                Stop();
+                Start((string)LbxProjects.SelectedItem);
+            }
         }
     }
 }
